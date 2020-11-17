07d774c950a8939c62d722fc159eb1ce4549b77b
mikhawa <michaeljpitz@gmail.com>
2020-11-17 13:50:28 +0100

    config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// constantes de connexion
+define("DB_HOST","localhost");
+define("DB_NAME","exemple_pdo");
+define("DB_LOGIN","root");
+define("DB_PWD","");
+define("DB_PORT",3306);
+define("DB_CHARSET","utf8");
+
+// est-on en production (true) ou en développement (false)
+define("PROD",false);
