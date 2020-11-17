940c2e370727106b8add407954e2531cf9c6385d
mikhawa <michaeljpitz@gmail.com>
2020-11-17 14:48:58 +0100

    04-select-PDO

---
+++ 04-select-PDO.php
@@ -0,0 +1,45 @@
+<?php
+// config+connexion
+require_once "03-connect-simple.php";
+
+// on récupère tous les users avec $connexion->query("SELECT");
+$recup_users = $connexion->query("SELECT * FROM users");
+
+// on récupère les valeurs formatée en tableau indexé contenant des tableaux associatif (dès que l'on doute que l'on peut avoir plus d'un élément, on utilise le fetchAll! Exception si on utilise une boucle while pour afficher le contenu )
+$users = $recup_users->fetchAll(PDO::FETCH_ASSOC);
+
+// pour connaître le nombre d'éléments récupérés
+
+$nb = $recup_users->rowCount();
+
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PDO - Query</title>
+</head>
+<body>
+<h1>PDO</h1>
+<h2>query</h2>
+<h3>On utilise InstancePDO->query("sql") pour les select</h3>
+<p>Contrairement à mysqli procédural, les requêtes d'un CRUD ne se font pas avec les mêmes méthodes</p>
+<code>$recup_users = $connexion->query("SELECT * FROM users");</code>
+<pre><?php var_dump($recup_users);?></pre>
+<code>$nb = $recup_users->rowCount();</code>
+<p><?=$nb?></p>
+<code>$users = $recup_users->fetchAll(PDO::FETCH_ASSOC);</code>
+<?php
+foreach($users as $item):
+?>
+<p><?=$item['thename']?></p>
+<?php
+endforeach;
+?>
+</body>
+</html>
