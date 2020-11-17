748e1aaaa80b1e5ecb64e151829577b8f043b4c3
mikhawa <michaeljpitz@gmail.com>
2020-11-17 14:05:34 +0100

    01-connect.php

---
+++ 02-connect-try-catch.php
@@ -0,0 +1,12 @@
+<?php
+// dependencies
+require_once "config.php";
+
+// on va instancier dans $connexion la classe native PDO (si l'extension est activée) avec le mot clef "new", suivi du nom de la classe et de ses arguments.
+$connexion = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET,DB_LOGIN,DB_PWD);
+?>
+<pre>
+    <?php
+    var_dump($connexion);
+    ?>
+</pre>
