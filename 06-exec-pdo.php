77646063a27c1219cc371b45fd42dad66d6fb4b2
mikhawa <michaeljpitz@gmail.com>
2020-11-17 16:33:23 +0100

    06-exec-pdo.php

--- 06-exec-pdo.php
+++ 06-exec-pdo.php
@@ -1,6 +1,20 @@
 <?php
 require_once "03-connect-simple.php";
 
+$sql=" INSERT INTO articles (thetitle,thetext,users_idusers) VALUES('coucou3','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel dapibus urna, scelerisque gravida lacus. Mauris at ligula purus. Nunc semper semper pellentesque. Duis at magna dapibus, lobortis lectus lacinia, pulvinar leo. Aliquam dignissim tellus nec eros bibendum dapibus. Nunc dapibus urna sit amet tincidunt condimentum. Cras eleifend a tortor at sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;Vivamus eget ligula nibh. Suspendisse dapibus quam euismod tortor facilisis condimentum. Duis rutrum tortor ut fermentum condimentum. Suspendisse vestibulum sapien est, vitae dignissim libero fringilla in. Phasellus pretium nibh ac enim tincidunt, nec tincidunt lectus tristique.',2);";
+
+// insertion
+$nb = $connexion->exec($sql);
+
+// récupération du dernier id inséré (par cet utilisateur, attention si on insert de multiples articles en une requête, il va nous donner le premier article de cette requête!!! https://www.php.net/manual/fr/pdo.lastinsertid.php)
+$lastid = $connexion->lastInsertId();
+
+
+// update
+$nbupdate = $connexion->exec("UPDATE articles SET thetext = 'lalala lala' WHERE idarticles > 7 ");
+
+// delete
+$nbdelete = $connexion->exec("DELETE FROM articles WHERE idarticles = 8");
 
 
 ?>
@@ -18,5 +32,7 @@ require_once "03-connect-simple.php";
 <h2>exec</h2>
 <h3>On utilise InstancePDO->exec("sql") pour les INSERT, UPDATE et DELETE</h3>
 <p>Contrairement à mysqli procédural, les requêtes d'un CRUD ne se font pas avec les mêmes méthodes</p>
+<p>$instancePDO->exec() va nous renvoyer le nombre de lignes affectées</p>
+<?php echo (isset($erreur))? $erreur : "$nb articles insérés dans la DB<br>$nbupdate article mis à jour<br>$nbdelete article supprimé" ?>
 </body>
 </html>
--- datas/nos-requetes.sql
+++ datas/nos-requetes.sql
@@ -3,3 +3,5 @@ FROM articles a
 	INNER JOIN users u
     ON a.users_idusers = u.idusers
  ORDER BY a.thedate DESC;   
+ 
+ INSERT INTO articles (thetitle,thetext,users_idusers) VALUES('coucou','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel dapibus urna, scelerisque gravida lacus. Mauris at ligula purus. Nunc semper semper pellentesque. Duis at magna dapibus, lobortis lectus lacinia, pulvinar leo. Aliquam dignissim tellus nec eros bibendum dapibus. Nunc dapibus urna sit amet tincidunt condimentum. Cras eleifend a tortor at sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam convallis ligula sed eros consequat, a venenatis mi posuere. Cras dignissim ante a sapien hendrerit, in porttitor ex pellentesque. Maecenas pellentesque ligula et enim pulvinar, vel maximus felis viverra. Vivamus eget ligula nibh. Suspendisse dapibus quam euismod tortor facilisis condimentum. Duis rutrum tortor ut fermentum condimentum. Suspendisse vestibulum sapien est, vitae dignissim libero fringilla in. Phasellus pretium nibh ac enim tincidunt, nec tincidunt lectus tristique.',1);
