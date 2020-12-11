b684f9349ece106cf02cddefeaf847282fc650ed
mikhawa <michaeljpitz@gmail.com>
2020-12-11 11:10:12 +0100

    05-delete

--- mysqli2PDO/05-delete-simple.php
+++ mysqli2PDO/05-delete-simple.php
@@ -1,7 +1,6 @@
 <?php
 require_once "config.php";
 require_once "01-connection.php";
-require_once "fonctions.php";
 
 // connexion via mysqli
 // $dbMysqli = connectMysqli();
@@ -10,25 +9,19 @@ require_once "fonctions.php";
 $dbPDO = connectPDO();
 
 /*
- * Mise à jour de certains articles
+ * suppression d'articles au hasard
  */
 
-// variable pour le between
-$debut = mt_rand(0,25);
-$fin = mt_rand(26,60);
+// variables pour supprimer maximum 3 articles (si ils existent)
+$un = mt_rand(1,83);
+$deux = mt_rand(1,83);
+$trois = mt_rand(1,83);
 
-// variable temporelle
-$thedate = date("Y-m-d H:i:s");
 
-// variable avec valeurs générées
-$newArticle = createDatasArticles();
-$newTitle = ucfirst($newArticle['thetitle']);
-$newText = ucfirst($newArticle['thetext']);
+$sql = "DELETE FROM articles WHERE idarticles IN ($un,$deux,$trois)";
 
-$sql = "UPDATE articles SET thetitle='$newTitle',thetext='$newText',thedate='$thedate' 
-        WHERE idarticles BETWEEN $debut AND $fin;";
-
-$nbUpdate = $dbPDO->exec($sql);
+// suppression de 0 à 3 articles
+$nbDelete = $dbPDO->exec($sql);
 
 /*
  * Récupération de tous les articles
@@ -55,11 +48,11 @@ $request->closeCursor();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Update au hasard de certains articles</title>
+    <title>Suppression au hasard de certains articles</title>
 </head>
 <body>
 <h2>Affichage de <?=$request->rowCount()?> article(s)</h2>
-<h3><?=$nbUpdate?> articles ont été modifiés au chargement de cette page</h3>
+<h3><?=$nbDelete?> articles ont été supprimés au chargement de cette page</h3>
 
 <?php
 foreach ($recup10LastArticles as $item):
