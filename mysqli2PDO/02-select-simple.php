7f456172580b3279228cc9276cc7877a67b2a62f
mikhawa <michaeljpitz@gmail.com>
2020-12-11 08:53:28 +0100

    mysqli-free_result et closeCursor()

--- mysqli2PDO/02-select-simple.php
+++ mysqli2PDO/02-select-simple.php
@@ -14,11 +14,14 @@ $sql = "SELECT * FROM users";
 // Requête en mysqli
 $requestMysqli = mysqli_query($dbMysqli,$sql);
 $resultMysqli = mysqli_fetch_all($requestMysqli,MYSQLI_ASSOC);
+// vide le jeu de résultats (optionnel,gagne un peu de mémoire)
+mysqli_free_result($requestMysqli);
 
 // Requête en PDO, on utilise $db->query QUE pour des SELECT SANS données utilisateurs
 $requestPDO =$dbPDO->query($sql);
 $resultPDO = $requestPDO->fetchAll(PDO::FETCH_ASSOC);
-
+// remet le jeu de résultat à son état initial (optionnel pour Mysql, mais utile pour d'autres type de DB donc la portabilité du code)
+$requestPDO->closeCursor();
 /*
  * version courte
  * $resultPDO = $dbPDO->query($sql)->fetchAll(PDO::FETCH_ASSOC);
